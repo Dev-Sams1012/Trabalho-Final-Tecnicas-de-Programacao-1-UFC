9c736da1047d68352bc14ef486d87ef2d60b449c
Dev-Sams1012 <samuel.augusto.abreu@gmail.com>
2025-07-19 11:48:08 -0300

    Acréscimo do método GetDataDeEmprestimo

--- Trabalho_Final_TP1_UFC/src/ufc/dc/tp1/app/itens/IEmprestavel.java
+++ Trabalho_Final_TP1_UFC/src/ufc/dc/tp1/app/itens/IEmprestavel.java
@@ -1,5 +1,6 @@
 package ufc.dc.tp1.app.itens;
 
+import java.time.LocalDate;
 import ufc.dc.tp1.app.exceptions.DevolucaoSemEmprestimoException;
 import ufc.dc.tp1.app.exceptions.VestimentaJaEmprestadoException;
 
@@ -7,8 +8,10 @@ public interface IEmprestavel {
 	public void registrarEmprestimo() throws VestimentaJaEmprestadoException;
 	
 	public int  quantidadeDeDiasDesdeOEmprestimo();
+
+	public LocalDate getDataDeEmprestimo();
 	
 	public void registrarDevolucao() throws DevolucaoSemEmprestimoException;
 	
 	public boolean isEmprestada();
-}
+} 
